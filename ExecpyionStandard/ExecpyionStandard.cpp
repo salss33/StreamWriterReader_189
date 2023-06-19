d9d42d80d91b8d25d957ebbaad6be0484fe3433e
salss33 <hallosalsa03@gmail.com>
2023-06-19 16:51:05 +0700

    buat membuka, menulis, membaca dan menutup file.

--- ExecpyionStandard/ExecpyionStandard.cpp
+++ ExecpyionStandard/ExecpyionStandard.cpp
@@ -1,20 +1,24 @@
-// ExecpyionStandard.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+#include <exception>
+//untuk obyek execption yang akan digunakan
+#include <array>
+//untuk obyek aray yang akan kita gunakan
+using namespace std;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	cout << "Awal Program" << endl; // Penanda 1:........
+	try {
+		array <int, 3> data = { 1, 2, 3 };
+		//pesan array integer 3 elemen
+		cout << data.at(4) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
